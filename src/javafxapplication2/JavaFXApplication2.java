3ea999c0b4ca1cba0d91bafc6ffc956e455e97a4
Ignacio-Rueda <nachord@outlook.com>
2024-04-01 13:56:06 +0200

    inicio saludo

--- src/javafxapplication2/JavaFXApplication2.java
+++ src/javafxapplication2/JavaFXApplication2.java
@@ -39,6 +39,7 @@ public class JavaFXApplication2 extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        System.out.println("hola");
     }
 
     /**
