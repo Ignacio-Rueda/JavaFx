2852085265e6ec1ad9183daba455d150fb794705
Ignacio-Rueda <nachord@outlook.com>
2024-04-01 20:13:20 +0200

    Lambda.

---
+++ src/ejemplofx/Lambda.java
@@ -0,0 +1,40 @@
+
+package ejemplofx;
+
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class Lambda extends Application{
+    @Override
+    public void start(Stage stage){
+        Button boton = new Button("Botón");
+        
+        boton.setOnAction(ActionEvent->{
+            System.out.println("holi");
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(boton);
+        
+        Scene scene = new Scene(root,300,300);
+        
+        stage.setTitle("LAMBDA");
+        stage.setScene(scene);
+        stage.show();
+       
+        
+    }
+    
+    
+    public static void main(String args[]){
+        launch(args);
+    }
+}
